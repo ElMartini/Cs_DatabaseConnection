8d6b6173a2ce2a1eae3596384302dd3406690d27
ElMartini <markam888@protonmail.com>
2025-11-27 13:17:29 +0100

    Updated project and added first unit test

--- Cs_DatabaseConnection/Model/AppDbContext.cs
+++ Cs_DatabaseConnection/Model/AppDbContext.cs
@@ -2,9 +2,10 @@
 
 namespace Cs_DatabaseConnection.Model;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext
+public class AppDbContext: DbContext
 {
-    
-    public DbSet<Parish> Parish { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options): base (options){}
+    public DbSet<Book> Books{ get; set; }
+    public DbSet<Author> Authors { get; set; }
     
 }
---
+++ Cs_DatabaseConnection/Model/Models.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cs_DatabaseConnection.Model;
+
+public class Book
+{
+    public int Id { get; set; }
+    [MaxLength(100)]
+    public required string Title { get; set; }
+    [MaxLength(100)]
+    public required string Author { get; set; }
+    public required int AuthorId { get; set; }
+}
+
+public class Author
+{
+    public int Id { get; set; }
+    [MaxLength(100)]
+    public required string FirstName { get; set; }
+    [MaxLength(100)]
+    public required string LastName { get; set; }
+}
